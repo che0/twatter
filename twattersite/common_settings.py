c0b309aeaef7710d6f4bf71a96312653ce2e932b
Petr Novak <pnovak@koukaam.se>
2012-09-19 16:05:44 +0200

    initial site

---
+++ twattersite/common_settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'south',
+)
